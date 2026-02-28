08282e2b5849b2ea83a7fe2ea9bf794859a72a97
Hasan-1000 <Hasan.alfaili@ucdenver.edu>
2026-02-27 23:50:04 -0700

    add

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var authController = require('./auth');
-var authJwtController = require('./auth_jwt');
+var authController = require('./auth');         // Basic Auth
+var authJwtController = require('./auth_jwt');  // JWT Auth
 db = require('./db')(); //hack
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
@@ -26,7 +26,8 @@ var router = express.Router();
 function getJSONObjectForMovieRequirement(req) {
     var json = {
         headers: "No headers",
-        key: process.env.UNIQUE_KEY,
+        query: "No query params",
+        env: process.env.UNIQUE_KEY,
         body: "No body"
     };
 
@@ -38,64 +39,90 @@ function getJSONObjectForMovieRequirement(req) {
         json.headers = req.headers;
     }
 
+    // req.query will exist; if no query params it will be {}
+    if (req.query != null && Object.keys(req.query).length > 0) {
+        json.query = req.query;
+    }
+
     return json;
 }
 
+// -------------------- /signup (POST only) --------------------
 router.post('/signup', (req, res) => {
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please include both username and password to signup.'})
+        return res.status(400).json({
+            success: false,
+            msg: 'Please include both username and password to signup.'
+        });
     } else {
         var newUser = {
             username: req.body.username,
             password: req.body.password
         };
 
-        db.save(newUser); //no duplicate checking
-        res.json({success: true, msg: 'Successfully created new user.'})
+        db.save(newUser); // no duplicate checking
+        return res.json({ success: true, msg: 'Successful created new user.' });
     }
 });
 
+router.all('/signup', (req, res) => {
+    res.status(405).send({ message: 'HTTP method not supported.' });
+});
+
+// -------------------- /signin (POST only) --------------------
 router.post('/signin', (req, res) => {
     var user = db.findOne(req.body.username);
 
     if (!user) {
-        res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
+        return res.status(401).send({
+            success: false,
+            msg: 'Authentication failed. User not found.'
+        });
     } else {
         if (req.body.password == user.password) {
             var userToken = { id: user.id, username: user.username };
             var token = jwt.sign(userToken, process.env.SECRET_KEY);
-            res.json ({success: true, token: 'JWT ' + token});
-        }
-        else {
-            res.status(401).send({success: false, msg: 'Authentication failed.'});
+            return res.json({ success: true, token: 'JWT ' + token });
+        } else {
+            return res.status(401).send({ success: false, msg: 'Authentication failed.' });
         }
     }
 });
 
-router.route('/testcollection')
-    .delete(authController.isAuthenticated, (req, res) => {
-        console.log(req.body);
-        res = res.status(200);
-        if (req.get('Content-Type')) {
-            res = res.type(req.get('Content-Type'));
-        }
+router.all('/signin', (req, res) => {
+    res.status(405).send({ message: 'HTTP method not supported.' });
+});
+
+// -------------------- /movies (GET/POST/PUT/DELETE only) --------------------
+router.route('/movies')
+    .get((req, res) => {
         var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "GET movies";
         res.json(o);
-    }
-    )
+    })
+    .post((req, res) => {
+        var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "movie saved";
+        res.json(o);
+    })
     .put(authJwtController.isAuthenticated, (req, res) => {
-        console.log(req.body);
-        res = res.status(200);
-        if (req.get('Content-Type')) {
-            res = res.type(req.get('Content-Type'));
-        }
         var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "movie updated";
         res.json(o);
-    }
-    );
-    
+    })
+    .delete(authController.isAuthenticated, (req, res) => {
+        var o = getJSONObjectForMovieRequirement(req);
+        o.status = 200;
+        o.message = "movie deleted";
+        res.json(o);
+    })
+    .all((req, res) => {
+        res.status(405).send({ message: 'HTTP method not supported.' });
+    });
+
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
-
-
